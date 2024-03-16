74cafee207bb148166c4e1826672516a5d1151c3
Victoria Trois <vickytrois@outlook.com>
2024-03-16 14:50:21 +0000

    Adiciona os scripts  e .

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Developing https://tabnews.com.br for professional growth objectives.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function Home() {
-    return <h1>Miga, eu implantei um site sozinha 🥳! </h1>
+  return <h1>Miga, eu implantei um site sozinha 🥳! </h1>;
 }
 
 export default Home;
