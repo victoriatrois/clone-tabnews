af6970efc82892986ad5daf207e473b6c80ed283
Victoria Trois <vickytrois@outlook.com>
2024-04-10 00:44:24 +0000

    Configures absolute path for folder .

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,15 +1,20 @@
-import database from "../../../../infra/database";
+import database from "infra/database";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function getStatus(
   request: NextApiRequest,
   response: NextApiResponse,
 ): Promise<void> {
-  const result = await database.query("SELECT 1+1 as Sum;");
-  console.log("===> result\n", result.rows);
-  response
-    .status(200)
-    .json({ resposta: "endpoint status enviando uma resposta." });
+  try {
+    const result = await database.query("SELECT 1+1 as Sum;");
+    console.log("===> result\n", result.rows);
+    response
+      .status(200)
+      .json({ resposta: "endpoint status enviando uma resposta." });
+  } catch (error) {
+    console.error(error);
+    response.status(500).json({ error: "Failed to connect to database" });
+  }
 }
 
 export default getStatus;
