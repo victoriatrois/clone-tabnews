c70b983dea71760d6c18395729ea84597b7182e6
Victoria Trois <vickytrois@outlook.com>
2024-04-12 22:21:33 +0000

    Finaliza o endpoint GET /status.

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,16 +1,53 @@
-import database from "/infra/database";
+import database from "infra/database";
 import { NextApiRequest, NextApiResponse } from "next";
+import { QueryResult } from "pg";
 
 async function getStatus(
   request: NextApiRequest,
   response: NextApiResponse,
 ): Promise<void> {
   try {
-    const result = await database.query("SELECT 1+1 as Sum;");
-    console.log("===> result\n", result.rows);
-    response
-      .status(200)
-      .json({ resposta: "endpoint status enviando uma resposta." });
+    const updatedAt: string = new Date().toISOString();
+
+    const databaseVersionQueryResult: QueryResult = await database.query(
+      "SHOW server_version;",
+    );
+    const databaseVersion: string =
+      databaseVersionQueryResult.rows[0].server_version;
+
+    const maxConnectionsQueryResult: QueryResult = await database.query(
+      "SHOW max_connections;",
+    );
+    const maxConnections: number = Number(
+      maxConnectionsQueryResult.rows[0].max_connections,
+    );
+
+    const databaseName: string = process.env.POSTGRES_DB;
+    if (!databaseName) {
+      throw new Error("POSTGRES_DB environment variable is not set");
+    }
+
+    const openedConnectionsQueryResult: QueryResult = await database.query({
+      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName],
+    });
+    const openedConnections: number = parseInt(
+      openedConnectionsQueryResult.rows[0].count,
+    );
+    if (isNaN(openedConnections)) {
+      throw new Error("Failed to parse opened connections count");
+    }
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          postgres_version: databaseVersion,
+          max_connections: maxConnections,
+          used_connections: openedConnections,
+        },
+      },
+    });
   } catch (error) {
     console.error(error);
     response.status(500).json({ error: "Failed to connect to database" });
--- tests/integration/api/v1/status/get.test.ts
+++ tests/integration/api/v1/status/get.test.ts
@@ -1,5 +1,36 @@
+interface DatabaseStatus {
+  postgres_version: string;
+  max_connections: number;
+  used_connections: number;
+}
+
+interface StatusResponse {
+  updated_at: string;
+  dependencies: {
+    database: DatabaseStatus;
+  };
+}
+
 test("GET /status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody: StatusResponse = await response.json();
+  const parsedUpdatedAt: string = new Date(
+    responseBody.updated_at
+  ).toISOString();
+  const postgresVersion: string = "16.0";
+  const maxConnections: number = 100;
+  const usedConnections: number = 1;
 
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toBeDefined();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+  expect(responseBody.dependencies.database.postgres_version).toEqual(
+    postgresVersion
+  );
+  expect(responseBody.dependencies.database.max_connections).toEqual(
+    maxConnections
+  );
+  expect(responseBody.dependencies.database.used_connections).toEqual(
+    usedConnections
+  );
 });
