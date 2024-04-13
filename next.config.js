5a6dc482a82129c4165ccf0f24183f27529d39e8
Victoria Trois <vickytrois@outlook.com>
2024-04-13 00:28:52 +0000

    Seccond attemmpt.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+module.exports = {
+  webpack: (config, options) => {
+    config.resolve.alias["infra"] = path.join(__dirname, "infra");
+    config.resolve.alias["infra/*"] = path.join(__dirname, "infra/*");
+    return config;
+  },
+};
