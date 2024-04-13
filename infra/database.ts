cf8b48275ddc0fdce6cbb17b196a09a1a9c5b45f
Victoria Trois <vickytrois@outlook.com>
2024-04-13 22:40:52 +0000

    Makes  more robust towards errors.

--- infra/database.ts
+++ infra/database.ts
@@ -8,13 +8,21 @@ async function query(queryObject: QueryConfig): Promise<any> {
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
   });
-  await client.connect();
+  console.log("credenciais do postgress:\n", {
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
