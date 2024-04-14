db6c2b8b306365b1d73ca9e2a2eba1ac4b712c7b
Victoria Trois <vickytrois@outlook.com>
2024-04-14 18:04:07 +0000

    Handles ssl database connection.

--- infra/database.ts
+++ infra/database.ts
@@ -7,6 +7,7 @@ async function query(queryObject: QueryConfig): Promise<any> {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("credenciais do postgress:\n", {
     host: process.env.POSTGRES_HOST,
