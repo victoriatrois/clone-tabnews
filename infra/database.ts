bee98127a64f628a9f8f37fa17c8af037604470b
Victoria Trois <vickytrois@outlook.com>
2024-04-14 20:25:54 +0000

    Removes some logs.

--- infra/database.ts
+++ infra/database.ts
@@ -9,13 +9,6 @@ async function query(queryObject: QueryConfig): Promise<any> {
     password: process.env.POSTGRES_PASSWORD,
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
-  console.log("credenciais do postgress:\n", {
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
 
   try {
     await client.connect();
