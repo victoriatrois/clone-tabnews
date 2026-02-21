6f4baf215484e7c94ac95a2d13814697158072ed
Victoria Trois <vickytrois@outlook.com>
2026-02-21 02:23:09 +0000

    refactor: use `next-connect` in `GET /status` and `/migrations` and in `POST /migrations`

--- pages/api/v1/migrations/index.ts
+++ pages/api/v1/migrations/index.ts
@@ -3,31 +3,33 @@ import migrationRunner, { RunnerOption } from "node-pg-migrate";
 import { resolve } from "path";
 import database from "../../../../infra/database";
 import { Client } from "pg";
+import { createRouter } from "next-connect";
+import controller from "infra/controller";
 
-export default async function migrations(
+const router = createRouter<NextApiRequest, NextApiResponse>();
+
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler(controller.errorHandler);
+
+let dbClient: Client;
+
+const databaseUrl: string = process.env.DATABASE_URL || "";
+const defaultMigrationOptions: RunnerOption = {
+  databaseUrl: databaseUrl as string,
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
+
+async function getHandler(
   request: NextApiRequest,
   response: NextApiResponse,
 ): Promise<void> {
-  const allowedMethods: string[] = ["GET", "POST"];
-  let dbClient: Client;
-  const databaseUrl: string = process.env.DATABASE_URL || "";
-
-  if (request.method && !allowedMethods.includes(request.method)) {
-    return response.status(405).json({
-      error: `Method ${request.method} not allowed.`,
-    });
-  }
-
   dbClient = await database.createDbClient();
-  const defaultMigrationOptions: RunnerOption = {
-    dbClient: dbClient,
-    databaseUrl: databaseUrl as string,
-    dryRun: true,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
 
   try {
     if (!databaseUrl) {
@@ -37,26 +39,45 @@ export default async function migrations(
       return;
     }
 
-    if (request.method === "GET") {
-      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
 
-      return response.status(200).json(pendingMigrations);
+    return response.status(200).json(pendingMigrations);
+  } finally {
+    await dbClient.end();
+  }
+}
+
+async function postHandler(
+  request: NextApiRequest,
+  response: NextApiResponse,
+): Promise<void> {
+  // let dbClient: Client;
+  // const databaseUrl: string = process.env.DATABASE_URL || "";
+
+  dbClient = await database.createDbClient();
+
+  try {
+    if (!databaseUrl) {
+      response
+        .status(500)
+        .json({ error: "DATABASE_URL environment variable is not set" });
+      return;
     }
 
-    if (request.method === "POST") {
-      const completedMigrations = await migrationRunner({
-        ...defaultMigrationOptions,
-        dryRun: false,
-      });
+    const completedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+      dbClient,
+    });
 
-      if (completedMigrations.length > 0) {
-        return response.status(201).json(completedMigrations);
-      }
-
-      return response.status(200).json(completedMigrations);
+    if (completedMigrations.length > 0) {
+      return response.status(201).json(completedMigrations);
     }
-  } catch (error) {
-    console.error(error);
+
+    return response.status(200).json(completedMigrations);
   } finally {
     await dbClient.end();
   }
